85fe3fd351ca348ef6cd9655bdecca7fa114f2c8
Leon Otto Gasteiger <leon.gasteiger@web.de>
2026-01-19 20:23:33 +0000

    Finished ingestion pipeline

---
+++ Workshop_Docker/pipeline/ingest_data.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+# # Ingesting Data into Postgres Database
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+year = 2021
+month = 1
+
+
+pg_user = 'root'
+pg_password = 'root'
+pg_host = 'localhost'
+pg_port = '5432'
+pg_db = 'ny_taxi'
+
+# Read a sample of the data
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+df = pd.read_csv(
+    url,
+    dtype=dtype,
+    parse_dates=parse_dates
+)
+
+
+engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+
+
+# ##### Importing Chunk-sized data
+
+
+def run():
+
+    #Names
+    year = 2021
+    month = 1
+
+    pg_user = 'root'
+    pg_password = 'root'
+    pg_host = 'localhost'
+    pg_port = '5432'
+    pg_db = 'ny_taxi'
+
+    cunksize = 100000
+
+    target_table = 'yellow_taxi_data'
+
+    #creation of chunk-sized iterator
+    
+    df_iter = pd.read_csv(
+    url,
+    dtype = dtype,
+    parse_dates = parse_dates,
+    iterator = True,
+    chunksize = cunksize
+    )
+
+    # inserting the data chunk by chunk into the database table with a progress bar
+    first = True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(n=0).to_sql(
+                name = target_table,
+                con = engine, 
+                if_exists = 'replace'
+            )
+            first = False
+        df_chunk.to_sql(
+            name = target_table, 
+            con = engine, 
+            if_exists = 'append'
+        )
+
+if __name__ == '__main__':
+    run()
+
+
+
+
+
+
+
+
+
+
+
