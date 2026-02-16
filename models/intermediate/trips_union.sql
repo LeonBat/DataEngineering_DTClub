71d22bb382a191ba35b7ed3c3ff77e76edbe0043
Leon Gasteiger <leon.gasteiger@web.de>
2026-02-16 15:16:37 +0000

    Minor Changes

--- models/intermediate/trips_union.sql
+++ models/intermediate/trips_union.sql
@@ -1,13 +1,13 @@
 with green_tripdata_partitioned as (
-    select * from {{ref('stg_green_tripdata_partitioned')}} 
+    select * from {{ref('stg_green_tripdata')}} 
 ),
 
  yellow_tripdata_partitioned as (
-    select * from {{ref('stg_yellow_tripdata_partitioned')}} 
+    select * from {{ref('stg_yellow_tripdata')}} 
 )
 
 trips_unioned as (
-    select * from green_tripdata_partitioned
+    select * from green_tripdata
     union all
-    select * from yellow_tripdata_partitioned
+    select * from yellow_tripdata
 )
